ab91bc734fee826cab3ef863839d411577d8873f
Wesley <wesleyyep@hotmail.com>
2014-12-21 09:32:42 +1200

    created

---
+++ src/ui/RobotSoccerMain.java
@@ -0,0 +1,13 @@
+package ui;
+
+import communication.*;
+
+public class RobotSoccerMain {
+
+	public static void main(String[] args) {
+		IReceiver receiver = new Receiver();
+		//probably need to make a swingworker for this?
+		receiver.accept(63400);
+	}
+
+}
