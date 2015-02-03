7367061f59813b1f8eb504ca618b2023517b24d3
Wesley <wesleyyep@hotmail.com>
2015-02-03 21:23:13 +1200

    added colour sampling for ball, team, ground, opponents

--- src/ui/ColourPanel.java
+++ src/ui/ColourPanel.java
@@ -15,68 +15,42 @@ import java.awt.image.BufferedImage;
  * Created by Wesley on 2/02/2015.
  */
 public class ColourPanel extends JPanel implements WebcamDisplayPanelListener {
-
-    private WebcamController webcamController;
-    private JButton sampleButton = new JButton("Start Sample");
-    private boolean isSampling = false;
-    private ColourSlider YSlider = new ColourSlider();
-    private ColourSlider USlider = new ColourSlider();
-    private ColourSlider VSlider = new ColourSlider();
+    SamplingPanel ballSamplingPanel;
+    SamplingPanel teamSamplingPanel;
+    SamplingPanel groundSamplingPanel;
+    SamplingPanel opponentSamplingPanel;
+    private SamplingPanel[] samplingPanels;
+    private JTabbedPane tabPane = new JTabbedPane();
 
     public ColourPanel(WebcamController wc) {
         this.setLayout(new MigLayout());
-        this.webcamController = wc;
-
-        USlider.setMin(-111);
-        USlider.setMax(111);
-        VSlider.setMin(-157);
-        VSlider.setMax(157);
-
-        add(sampleButton, "wrap");
-        add(YSlider, "wrap, width 400:400:400");
-        add(USlider, "wrap, width 400:400:400");
-        add(VSlider, "wrap, width 400:400:400");
-
-        sampleButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (!isSampling) {
-                    sampleButton.setText("Stop Sample");
-                    isSampling = true;
-                } else {
-                    sampleButton.setText("Start Sample");
-                }
-            }
-        });
+        ballSamplingPanel = new SamplingPanel(wc);
+        teamSamplingPanel = new SamplingPanel(wc);
+        groundSamplingPanel = new SamplingPanel(wc);
+        opponentSamplingPanel = new SamplingPanel(wc);
+        samplingPanels = new SamplingPanel[] { ballSamplingPanel, teamSamplingPanel, groundSamplingPanel, opponentSamplingPanel };
+        tabPane.addTab("Ball", ballSamplingPanel);
+        tabPane.addTab("Team", teamSamplingPanel);
+        tabPane.addTab("Ground", groundSamplingPanel);
+        tabPane.addTab("Opponent", opponentSamplingPanel);
+        add(tabPane);
     }
 
     public void takeSample(double xPos, double yPos) {
-//        System.out.println("X: " + x);
-//        System.out.println(("Y: " + y));
-        BufferedImage image = webcamController.getImageFromWebcam();
-        System.out.println(image.getWidth());
-        System.out.println(image.getHeight());
-        Color color = new Color(image.getRGB((int)xPos, (int)yPos));
-
-        int r = color.getRed();
-        int g = color.getGreen();
-        int b = color.getBlue();
-
-        int y = (int)(0.299 * r + 0.587 * g + 0.114 * b);
-        int u = (int)(-0.14713 * r + -0.28886 * g + 0.436 * b);
-        int v = (int)(0.615 * r + -0.51499 * g + -0.10001 * b);
-
-        YSlider.addToData(y);
-        USlider.addToData(u);
-        VSlider.addToData(v);
-
-        System.out.println("Y: " + y);
-        System.out.println("U: " + u);
-        System.out.println("V: " + v);
+        for (SamplingPanel sp : samplingPanels) {
+            if (sp.isSampling) {
+                sp.takeSample(xPos, yPos);
+            }
+        }
     }
 
     public boolean getIsSampling() {
-        return isSampling;
+        for (SamplingPanel sp : samplingPanels) {
+            if (sp.isSampling) {
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
---
+++ src/ui/SamplingPanel.java
@@ -0,0 +1,78 @@
+package ui;
+
+import controllers.WebcamController;
+import net.miginfocom.swing.MigLayout;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+
+/**
+ * Created by Wesley on 3/02/2015.
+ */
+public class SamplingPanel extends JPanel {
+
+    private WebcamController webcamController;
+    private ColourSlider YSlider = new ColourSlider();
+    private ColourSlider USlider = new ColourSlider();
+    private ColourSlider VSlider = new ColourSlider();
+    private JButton sampleButton = new JButton("Start Sample");
+    public boolean isSampling = false;
+
+    public SamplingPanel (WebcamController wc) {
+        this.setLayout(new MigLayout());
+        this.webcamController = wc;
+
+        USlider.setMin(-111);
+        USlider.setMax(111);
+        VSlider.setMin(-157);
+        VSlider.setMax(157);
+
+        add(sampleButton, "wrap");
+        add(YSlider, "wrap, width 400:400:400");
+        add(USlider, "wrap, width 400:400:400");
+        add(VSlider, "wrap, width 400:400:400");
+
+        sampleButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (!isSampling) {
+                    sampleButton.setText("Stop Sample");
+                    isSampling = true;
+                } else {
+                    sampleButton.setText("Start Sample");
+                    isSampling = false;
+                }
+            }
+        });
+
+    }
+
+    public void takeSample(double xPos, double yPos) {
+//        System.out.println("X: " + x);
+//        System.out.println(("Y: " + y));
+        BufferedImage image = webcamController.getImageFromWebcam();
+        System.out.println(image.getWidth());
+        System.out.println(image.getHeight());
+        Color color = new Color(image.getRGB((int)xPos, (int)yPos));
+
+        int r = color.getRed();
+        int g = color.getGreen();
+        int b = color.getBlue();
+
+        int y = (int)(0.299 * r + 0.587 * g + 0.114 * b);
+        int u = (int)(-0.14713 * r + -0.28886 * g + 0.436 * b);
+        int v = (int)(0.615 * r + -0.51499 * g + -0.10001 * b);
+
+        YSlider.addToData(y);
+        USlider.addToData(u);
+        VSlider.addToData(v);
+
+        System.out.println("Y: " + y);
+        System.out.println("U: " + u);
+        System.out.println("V: " + v);
+    }
+
+}
