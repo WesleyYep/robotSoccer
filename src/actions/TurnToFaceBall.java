89ee940a7a61bbaccee92aebf74b74bfb7022004
Wesley <wesleyyep@hotmail.com>
2015-02-04 22:19:53 +1200

    added some robot/ball pixel range sliders

---
+++ src/actions/TurnToFaceBall.java
@@ -0,0 +1,50 @@
+package actions;
+
+import Paths.StraightLinePath;
+import bot.Robot;
+import strategy.Action;
+
+/**
+ * Created by Wesley on 21/01/2015.
+ */
+public class TurnToFaceBall extends Action{
+    @Override
+    public String getName() {
+        return "Turn to ball (trial)";
+    }
+
+    @Override
+    public void execute() {
+        Robot r = bots.getRobot(index);
+
+        double ballTheta = Math.atan2(r.getYPosition() - ballY, ballX - r.getXPosition());
+        double difference = ballTheta - Math.toRadians(r.getTheta());
+        //some hack to make the difference -Pi < theta < Pi
+        if (difference > Math.PI) {
+            difference -= (2 * Math.PI);
+        } else if (difference < -Math.PI) {
+            difference += (2 * Math.PI);
+        }
+
+        double errorMargin = 0.7;
+        if (Math.abs(difference) >= errorMargin) {
+            if (difference > 0) {
+                r.angularVelocity = 2*Math.PI;
+            } else {
+                r.angularVelocity = -2*Math.PI;
+            }
+            r.linearVelocity = 0;
+        } else {
+            r.angularVelocity = 0;
+            r.linearVelocity = 0;
+        }
+
+
+    //    System.out.println(System.currentTimeMillis());
+
+    }
+
+    protected double squared (double x) {
+        return x * x;
+    }
+}
--- src/ui/ColourPanel.java
+++ src/ui/ColourPanel.java
@@ -6,6 +6,8 @@ import controllers.WebcamController;
 import net.miginfocom.swing.MigLayout;
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -20,6 +22,11 @@ public class ColourPanel extends JPanel implements WebcamDisplayPanelListener {
     SamplingPanel groundSamplingPanel;
     SamplingPanel opponentSamplingPanel;
     private SamplingPanel[] samplingPanels;
+    private RangeSlider robotSizeSlider = new RangeSlider(0, 500);
+    private RangeSlider ballSizeSlider = new RangeSlider(0, 500);
+    private JLabel robotSizeLabel = new JLabel("0 : 500");
+    private JLabel ballSizeLabel = new JLabel("0 : 500");
+
     private JTabbedPane tabPane = new JTabbedPane();
 
     public ColourPanel(WebcamController wc) {
@@ -33,7 +40,27 @@ public class ColourPanel extends JPanel implements WebcamDisplayPanelListener {
         tabPane.addTab("Team", teamSamplingPanel);
         tabPane.addTab("Ground", groundSamplingPanel);
         tabPane.addTab("Opponent", opponentSamplingPanel);
-        add(tabPane);
+        add(tabPane, "wrap");
+        add(new JLabel("Robot Pixel Range"), "wrap");
+        add(robotSizeSlider, "wrap");
+        add(robotSizeLabel, "wrap");
+        add(new JLabel("Ball Pixel Range"), "wrap");
+        add(ballSizeSlider, "wrap");
+        add(ballSizeLabel, "wrap");
+
+        robotSizeSlider.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                robotSizeLabel.setText(robotSizeSlider.getLowValue() + " : " + robotSizeSlider.getHighValue());
+            }
+        });
+
+        ballSizeSlider.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                ballSizeLabel.setText(ballSizeSlider.getLowValue() + " : " + ballSizeSlider.getHighValue());
+            }
+        });
     }
 
     public void takeSample(double xPos, double yPos) {
