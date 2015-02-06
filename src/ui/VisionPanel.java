1d90d05a1c0dcfc7e6f86a16fdff5366aaaa8a87
Wal8800 <johnlaw1234@hotmail.com>
2015-02-06 22:15:37 +1300

    still some +- error but work with rotated, working on curve edges

--- src/ui/VisionPanel.java
+++ src/ui/VisionPanel.java
@@ -1,14 +1,19 @@
 package ui;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
-import javax.swing.JTextField;
 
 import net.miginfocom.swing.MigLayout;
 import controllers.WebcamController;
@@ -59,15 +64,11 @@ public class VisionPanel extends JPanel implements WebcamDisplayPanelListener{
 	private double xScaleFactor = 0;
 	private double yScaleFactor = 0;
 	
-	private int topY;
-	private int BottomY;
-	private int centreX;
+	private boolean isDrawImage = false;
 	
-	private int leftX;
-	private int centreY;
-	private int rightX;
 	
 	public VisionPanel(WebcamController wc) {
+		
 		webcamController = wc;
 		
 		topRightButton = new JButton("Top Right");
@@ -325,7 +326,7 @@ public class VisionPanel extends JPanel implements WebcamDisplayPanelListener{
 	}
 	
 	
-	public void updateMousePoint(int x, int y) {
+	public void updateMousePoint(int x, int y, BufferedImage image) {
 		mousePoint.setText("Mouse at x:" + x + " y: " + y);
 		
 		if (buttonSelected != null) {
@@ -376,9 +377,10 @@ public class VisionPanel extends JPanel implements WebcamDisplayPanelListener{
 			System.out.println(rightAbove+ " " + right +" " + rightBelow);
 			System.out.println(bottomLeft + " " + bottomMiddle + " " + bottomRight);
 			
+			
 			if (topLeft != null && topMiddle !=null && topRight !=null) {
 				if (bottomLeft != null && bottomMiddle != null && bottomRight != null) {
-						
+						/*Method 1
 						topY = Math.round((topLeft.y+topMiddle.y+topRight.y)/3);
 						BottomY = Math.round((bottomLeft.y+bottomMiddle.y+bottomRight.y)/3);
 						//centreX = Math.round((left.x + right.x)/2);
@@ -394,12 +396,20 @@ public class VisionPanel extends JPanel implements WebcamDisplayPanelListener{
 						System.out.println("topY: " +  topY + " BottomY: " + BottomY);
 						System.out.println("leftX: " + leftX + " rightX: " + rightX);
 						System.out.println("xscale: " + xScaleFactor + " yscale: " + yScaleFactor);
+						
+					
+						if (leftAbove != null && leftBelow != null && rightAbove != null && rightBelow != null) {
+							
+						}
+						*/
 					}	
 			}
+			
 		}
 		
-		
+		/*Method 1
 		if (topY != 0 && leftX != 0) {
+			
 			double actualX = x-leftX;
 			double actualY = y-topY;
 			
@@ -407,6 +417,201 @@ public class VisionPanel extends JPanel implements WebcamDisplayPanelListener{
 			actualY = actualY/yScaleFactor;
 			
 			System.out.println("Actual X:" + actualX + " actual y:" + actualY);
+			
+		}*/
+		
+		if (topLeft != null && topRight !=null) {
+			if (bottomLeft != null && bottomRight != null) {	
+				System.out.println("");
+				//positive x axis as 0
+				double angleX1 = Math.atan(topRight.getY()-topLeft.getY())/(topRight.getX()-topLeft.getX());
+				double angleX2 = Math.atan(bottomRight.getY()-bottomLeft.getY())/(bottomRight.getX()-bottomLeft.getX());
+				
+				double avgAngleX = (angleX1+angleX2)/2;
+				System.out.println("angle x:" + Math.toDegrees(avgAngleX));
+			//	System.out.println(angleX1);
+			//	System.out.println(angleX2);
+				
+				//positve y axis as 0
+				double angleY1 = Math.atan(topRight.getX()-bottomRight.getX())/(topRight.getY()-bottomRight.getY());
+				double angleY2 = Math.atan(topLeft.getX()-bottomLeft.getX())/(topLeft.getY()-bottomLeft.getY());
+				
+				double avgAngleY = (angleY1+angleY2)/2;
+			//	System.out.println(angleY1);
+			//	System.out.println(angleY2);
+				
+				System.out.println("angle y:" + Math.toDegrees(avgAngleY));
+				
+				double avgAngle = -1*(avgAngleY+ (-1*avgAngleX)) /2;
+				//double avgAngle = (-1*(2*Math.PI)/360)*1.5;
+				System.out.println("avgAngle:" + Math.toDegrees(avgAngle));
+				
+				
+				double x1 = (topLeft.getX()+topRight.getX())/2;
+				double y1 = (topLeft.getY()+topRight.getY())/2;
+				
+				double x2 = (bottomLeft.getX()+bottomRight.getX())/2;
+				double y2 = (bottomLeft.getY()+bottomRight.getY())/2;
+				
+				double x3 = (topLeft.getX()+bottomLeft.getX())/2;
+				double y3 = (topLeft.getY()+bottomLeft.getY())/2;
+				
+				double x4 = (topRight.getX()+bottomRight.getX())/2;
+				double y4 = (topRight.getY()+bottomRight.getY())/2;
+				
+				double d = (x1-x2)*(y3-y4) - (y1-y2)*(x3-x4);  
+			    double xi = ((x3-x4)*(x1*y2-y1*x2)-(x1-x2)*(x3*y4-y3*x4))/d;
+			    double yi = ((y3-y4)*(x1*y2-y1*x2)-(y1-y2)*(x3*y4-y3*x4))/d;
+				
+				AffineTransform t = new AffineTransform();
+				t.rotate((2*Math.PI)/90,xi,yi);
+				
+				
+				Point newTopLeft = new Point();
+				Point newTopRight = new Point();
+				Point newBottomRight = new Point();
+				Point newBottomLeft = new Point();
+				
+				Point selectedPoint = new Point();
+				
+				Point[] oldPoint = {topLeft,topRight,bottomRight,bottomLeft};
+				Point[] newPoint = {newTopLeft,newTopRight,newBottomRight,newBottomLeft};
+				
+				t.transform(oldPoint, 0, newPoint, 0, 4);
+				
+				t.transform(new Point(x,y), selectedPoint);
+				
+				double avgTopLine = (topLeft.getY() + topRight.getY())/2;
+				double avgBotLine = (bottomLeft.getY()+bottomRight.getY())/2;
+				
+				double avgRightLine = (topRight.getX()+bottomRight.getX())/2;
+				double avgLeftLine = (topLeft.getX()+bottomLeft.getX())/2;
+				
+				double yScaleFactor = (avgBotLine - avgTopLine)/180.00;
+				double xScaleFactor = (avgRightLine-avgLeftLine)/220.00;
+				
+				double actualX = (selectedPoint.getX()-avgLeftLine)/xScaleFactor;
+				double actualY = (selectedPoint.getY()-avgTopLine)/yScaleFactor;
+					
+				
+				System.out.println(actualX + " " + actualY);
+				/*
+				double newTopRightY = (-topRight.getX()*Math.sin(avgAngle)) + (topRight.getY()*Math.cos(avgAngle));
+				double newTopLeftY= (-topLeft.getX()*Math.sin(avgAngle)) + (topLeft.getY()*Math.cos(avgAngle));
+				
+				double newBottomRightY = (-bottomRight.getX()*Math.sin(avgAngle)) + (bottomRight.getY()*Math.cos(avgAngle));
+				double newBottomLeftY = (-bottomLeft.getX()*Math.sin(avgAngle)) + (bottomLeft.getY()*Math.cos(avgAngle));
+				
+				double newTopRightX = (topRight.getX()*Math.cos(avgAngle)) + (topRight.getY()*Math.sin(avgAngle));
+				double newBottomRightX = (bottomRight.getX()*Math.cos(avgAngle)) + (bottomRight.getY()*Math.sin(avgAngle));
+				
+				double newBottomLeftX = (bottomLeft.getX()*Math.cos(avgAngle)) + (bottomLeft.getY()*Math.sin(avgAngle));
+				double newTopLeftX= (topLeft.getX()*Math.cos(avgAngle)) + (topLeft.getY()*Math.sin(avgAngle));
+				
+
+				double newTopRightY = (-topRight.getX()*Math.sin(avgAngle)) + (topRight.getY()*Math.cos(avgAngle));
+				double newTopLeftY= (-topLeft.getX()*Math.sin(avgAngle)) + (topLeft.getY()*Math.cos(avgAngle));
+				
+				double newBottomRightY = (-bottomRight.getX()*Math.sin(avgAngle)) + (bottomRight.getY()*Math.cos(avgAngle));
+				double newBottomLeftY = (-bottomLeft.getX()*Math.sin(avgAngle)) + (bottomLeft.getY()*Math.cos(avgAngle));
+				
+				double newTopRightX = (topRight.getX()*Math.cos(avgAngle)) + (topRight.getY()*Math.sin(avgAngle));
+				double newBottomRightX = (bottomRight.getX()*Math.cos(avgAngle)) + (bottomRight.getY()*Math.sin(avgAngle));
+				
+				double newBottomLeftX = (bottomLeft.getX()*Math.cos(avgAngle)) + (bottomLeft.getY()*Math.sin(avgAngle));
+				double newTopLeftX= (topLeft.getX()*Math.cos(avgAngle)) + (topLeft.getY()*Math.sin(avgAngle));
+				
+				System.out.println("newTopRightX:" + newTopRightX);
+				System.out.println("newBottomRightX" + newBottomRightX);
+				
+				double avgTopYLine = (newTopRightY+newTopLeftY)/2;
+				double avgBotYLine = (newBottomRightY+newBottomLeftY)/2;
+				double yScaleFactor = (avgBotYLine-avgTopYLine)/180.00;
+				
+				double avgLeftXLine = (newTopLeftX+newBottomLeftX)/2;
+				double avgRightXLine = (newTopRightX+newBottomRightX)/2;
+				double xScaleFactor = (avgRightXLine-avgLeftXLine)/220.00;
+				
+				System.out.println(newTopRightY + " " + newTopLeftY);
+				System.out.println(avgTopYLine + " " + avgBotYLine + " " + yScaleFactor);
+				System.out.println(avgLeftXLine + " " + avgRightXLine + " " + xScaleFactor);
+				
+				
+				double newX = (x*Math.cos(avgAngle)) + (y*Math.sin(avgAngle));
+				double newY = (-x*Math.sin(avgAngle)) + (y*Math.cos(avgAngle));
+				
+				newX = (newX - avgLeftXLine)/xScaleFactor;
+				newY = (newY - avgTopYLine)/yScaleFactor;
+				
+				System.out.println("actual x:" + newX);
+				System.out.println("actual y:" + newY);		
+				
+				*/
+				Graphics2D g2d = image.createGraphics();
+				
+				/*
+				double oldTopLeftY = (avgLeftXLine*Math.sin(avgAngle)) + (avgTopYLine*Math.cos(avgAngle));
+				double oldTopRightY = (avgRightXLine*Math.sin(avgAngle)) + (avgTopYLine*Math.cos(avgAngle));
+				
+				double oldTopLeftX = (avgLeftXLine*Math.cos(avgAngle)) - (avgTopYLine*Math.sin(avgAngle));
+				double oldTopRightX = (avgRightXLine*Math.cos(avgAngle)) - (avgTopYLine*Math.sin(avgAngle));
+				*/
+				g2d.setColor(Color.black);
+				
+				//axis
+				g2d.drawLine(image.getWidth()/2, 0, image.getWidth()/2, image.getHeight());
+				g2d.drawLine(0, image.getHeight()/2, image.getWidth(), image.getHeight()/2);
+				
+				//orginally rectangle
+				g2d.drawLine(topLeft.x, topLeft.y, topRight.x, topRight.y);
+				g2d.drawLine(topLeft.x, topLeft.y, bottomLeft.x, bottomLeft.y);
+				g2d.drawLine(topRight.x,topRight.y,bottomRight.x,bottomRight.y);
+				g2d.drawLine(bottomLeft.x,bottomLeft.y,bottomRight.x,bottomRight.y);
+				
+				g2d.setColor(Color.BLUE);
+				//after rotation
+				g2d.drawLine(newTopLeft.x,newTopLeft.y,newTopRight.x,newTopRight.y);
+				g2d.drawLine(newTopLeft.x,newTopLeft.y,newBottomLeft.x,newBottomLeft.y);
+				g2d.drawLine(newTopRight.x,newTopRight.y,newBottomRight.x,newBottomRight.y);
+				g2d.drawLine(newBottomLeft.x,newBottomLeft.y,newBottomRight.x,newBottomRight.y);
+				
+				g2d.setColor(Color.red);
+				//avg lines;
+				g2d.drawLine((int)Math.round(avgLeftLine),(int) Math.round(avgTopLine),(int) Math.round(avgRightLine),(int) Math.round(avgTopLine));
+				g2d.drawLine((int)Math.round(avgLeftLine),(int) Math.round(avgTopLine),(int) Math.round(avgLeftLine),(int) Math.round(avgBotLine));
+				g2d.drawLine((int)Math.round(avgRightLine),(int) Math.round(avgTopLine),(int) Math.round(avgRightLine),(int) Math.round(avgBotLine));
+				g2d.drawLine((int)Math.round(avgLeftLine),(int) Math.round(avgBotLine),(int) Math.round(avgRightLine),(int) Math.round(avgBotLine));
+				
+				g2d.setColor(Color.green);
+				System.out.println(selectedPoint);
+				g2d.fillRect((int)Math.round(selectedPoint.getX()), (int)Math.round(selectedPoint.getY()), 5, 5);
+				
+				g2d.setColor(Color.MAGENTA);
+				g2d.fillRect(x,y,5,5);
+				/*
+				g2d.drawLine((int)Math.round(newTopLeftX),(int) Math.round(newTopLeftY),(int) Math.round(newTopRightX),(int) Math.round(newTopRightY));
+				g2d.drawLine((int)Math.round(newTopLeftX),(int) Math.round(newTopLeftY),(int) Math.round(newBottomLeftX),(int) Math.round(newBottomLeftY));
+				g2d.drawLine((int)Math.round(newTopRightX),(int) Math.round(newTopRightY),(int) Math.round(newBottomRightX),(int) Math.round(newBottomRightY));
+				g2d.drawLine((int)Math.round(newBottomLeftX),(int) Math.round(newBottomLeftY),(int) Math.round(newBottomRightX),(int) Math.round(newBottomRightY));
+				
+				g2d.drawLine((int)Math.round(oldTopLeftX), 
+						(int)Math.round(oldTopLeftY), 
+						(int)Math.round(oldTopRightX), 
+						(int)Math.round(oldTopRightY));
+				*/
+				
+				/*
+				g2d.drawLine((int)Math.round(avgLeftXLine), 
+						(int)Math.round(avgTopYLine), 
+						(int)Math.round(avgRightXLine), 
+						(int)Math.round(avgTopYLine));
+						
+				*/
+			
+				ImageIcon ii = new ImageIcon(image);
+				JOptionPane.showMessageDialog(null,ii);
+					
+				}	
 		}
 		
 		
@@ -436,4 +641,5 @@ public class VisionPanel extends JPanel implements WebcamDisplayPanelListener{
 	public void viewStateChanged() {
 		
 	}
+
 }
