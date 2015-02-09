283cc2872ea1deb8ed7f5481e024c5c8883d6f3a
Wal8800 <johnlaw1234@hotmail.com>
2015-02-09 14:29:17 +1300

    use perspective transform instead

--- src/ui/VisionPanel.java
+++ src/ui/VisionPanel.java
@@ -6,9 +6,12 @@ import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.geom.AffineTransform;
+import java.awt.geom.NoninvertibleTransformException;
 import java.awt.geom.Point2D;
+import java.awt.geom.Point2D.Double;
 import java.awt.image.BufferedImage;
 
+import javax.media.jai.PerspectiveTransform;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -25,11 +28,9 @@ public class VisionPanel extends JPanel implements WebcamDisplayPanelListener{
 	
 	private Point2D topRight;
 	private Point2D topLeft;
-	private Point topMiddle;
 	
 	private Point2D bottomRight;
 	private Point2D bottomLeft;
-	private Point bottomMiddle;
 	
 	private WebcamController webcamController;
 	
@@ -37,16 +38,21 @@ public class VisionPanel extends JPanel implements WebcamDisplayPanelListener{
 	
 	private JLabel mousePoint;
 	
-	private JButton buttonSelected = null;
-	private String originalButtonText = null;
-	
 	private boolean isDrawImage = false;
 	
 	private BoardDialog dialog;
 	private BufferedImage webcamImage;
 	
+	
+	private double mapLeft = 121;
+	private double mapRight = 517;
+	private double mapTop = 48;
+	private double mapBot = 372;
+	
+	private PerspectiveTransform t;
+	private PerspectiveTransform tInverse;
+	
 	public VisionPanel(WebcamController wc) {
-		
 		topRight = new Point2D.Double(200,100);
 		topLeft = new Point2D.Double(100,100);
 		bottomLeft = new Point2D.Double(100,200);
@@ -78,6 +84,7 @@ public class VisionPanel extends JPanel implements WebcamDisplayPanelListener{
 		});
 		
 		
+		createTransformMatrix();
 		mousePoint = new JLabel("Mouse at x: 0 y: 0");
 		
 		this.setLayout(new MigLayout());
@@ -93,144 +100,28 @@ public class VisionPanel extends JPanel implements WebcamDisplayPanelListener{
 			if (bottomLeft != null && bottomRight != null) {	
 				System.out.println("");
 				//positive x axis as 0
-				double angleX1 = Math.atan(topRight.getY()-topLeft.getY())/(topRight.getX()-topLeft.getX());
-				double angleX2 = Math.atan(bottomRight.getY()-bottomLeft.getY())/(bottomRight.getX()-bottomLeft.getX());
-				
-				double avgAngleX = (angleX1+angleX2)/2;
-				System.out.println("angle x:" + Math.toDegrees(avgAngleX));
-				System.out.println(Math.toDegrees(angleX1));
-				System.out.println(Math.toDegrees(angleX2));
-				
-				//positve y axis as 0
-				double angleY1 = Math.atan(topRight.getX()-bottomRight.getX())/(topRight.getY()-bottomRight.getY());
-				double angleY2 = Math.atan(topLeft.getX()-bottomLeft.getX())/(topLeft.getY()-bottomLeft.getY());
-				
-				double avgAngleY = (angleY1+angleY2)/2;
-				
-				
-				System.out.println("angle y:" + Math.toDegrees(avgAngleY));
-				System.out.println(Math.toDegrees(angleY1));
-				System.out.println(Math.toDegrees(angleY2));
-				double avgAngle = -1*(avgAngleY+ (-1*avgAngleX)) /2;
-				//double avgAngle = (-1*(2*Math.PI)/360)*1.5;
-				System.out.println("avgAngle:" + Math.toDegrees(avgAngle));
-				
-				
-				double x1 = (topLeft.getX()+topRight.getX())/2;
-				double y1 = (topLeft.getY()+topRight.getY())/2;
-				
-				double x2 = (bottomLeft.getX()+bottomRight.getX())/2;
-				double y2 = (bottomLeft.getY()+bottomRight.getY())/2;
-				
-				double x3 = (topLeft.getX()+bottomLeft.getX())/2;
-				double y3 = (topLeft.getY()+bottomLeft.getY())/2;
-				
-				double x4 = (topRight.getX()+bottomRight.getX())/2;
-				double y4 = (topRight.getY()+bottomRight.getY())/2;
-				
-				double d = (x1-x2)*(y3-y4) - (y1-y2)*(x3-x4);  
-			    double xi = ((x3-x4)*(x1*y2-y1*x2)-(x1-x2)*(x3*y4-y3*x4))/d;
-			    double yi = ((y3-y4)*(x1*y2-y1*x2)-(y1-y2)*(x3*y4-y3*x4))/d;
-				
-				AffineTransform t = new AffineTransform();
-				t.rotate(avgAngle,xi,yi);
-				
-				
-				Point2D newTopLeft = new Point2D.Double();
-				Point2D newTopRight = new Point2D.Double();
-				Point2D newBottomRight = new Point2D.Double();
-				Point2D newBottomLeft = new Point2D.Double();
+				createTransformMatrix();
 				
 				Point2D selectedPoint = new Point2D.Double();
-				
-				Point2D[] oldPoint = {topLeft,topRight,bottomRight,bottomLeft};
-				Point2D[] newPoint = {newTopLeft,newTopRight,newBottomRight,newBottomLeft};
-				
-				t.transform(oldPoint, 0, newPoint, 0, 4);
-				
 				t.transform(new Point2D.Double(x,y), selectedPoint);
-
-				double avgTopLine = (newTopLeft.getY() + newTopRight.getY())/2;
-				double avgBotLine = (newBottomLeft.getY()+newBottomRight.getY())/2;
-				
-				double avgRightLine = (newTopRight.getX()+newBottomRight.getX())/2;
-				double avgLeftLine = (newTopLeft.getX()+newBottomLeft.getX())/2;
-				
-				double yScaleFactor = (avgBotLine - avgTopLine)/180.00;
-				double xScaleFactor = (avgRightLine-avgLeftLine)/220.00;
-				
-				double actualX = (selectedPoint.getX()-avgLeftLine)/xScaleFactor;
-				double actualY = (selectedPoint.getY()-avgTopLine)/yScaleFactor;
-					
-				
-				System.out.println(actualX + " " + actualY);
-				
-
-				Graphics2D g2d = image.createGraphics();
-				
-				g2d.setColor(Color.black);
-				
-				//axis
-				g2d.drawLine(image.getWidth()/2, 0, image.getWidth()/2, image.getHeight());
-				g2d.drawLine(0, image.getHeight()/2, image.getWidth(), image.getHeight()/2);
-				
-				//orginally rectangle
-				g2d.drawLine((int)Math.round(topLeft.getX())
-						, (int)Math.round(topLeft.getY())
-						, (int)Math.round(topRight.getX())
-						,(int)Math.round(topRight.getY()));
-				
-				g2d.drawLine((int)Math.round(topLeft.getX())
-						, (int)Math.round(topLeft.getY())
-						, (int)Math.round(bottomLeft.getX())
-						, (int)Math.round(bottomLeft.getY()));
-				
-				g2d.drawLine((int)Math.round(topRight.getX())
-						,(int)Math.round(topRight.getY())
-						,(int)Math.round(bottomRight.getX())
-						,(int)Math.round(bottomRight.getY()));
-				
-				g2d.drawLine((int)Math.round(bottomLeft.getX())
-						,(int)Math.round(bottomLeft.getY())
-						,(int)Math.round(bottomRight.getX())
-						,(int)Math.round(bottomRight.getY()));
-				
-				g2d.setColor(Color.BLUE);
-				//after rotation
-				g2d.drawLine((int)Math.round(newTopLeft.getX())
-						,(int)Math.round(newTopLeft.getY())
-						,(int)Math.round(newTopRight.getX())
-						,(int)Math.round(newTopRight.getY()));
-				g2d.drawLine((int)Math.round(newTopLeft.getX())
-						,(int)Math.round(newTopLeft.getY())
-						,(int)Math.round(newBottomLeft.getX())
-						,(int)Math.round(newBottomLeft.getY()));
-				g2d.drawLine((int)Math.round(newTopRight.getX())
-						,(int)Math.round(newTopRight.getY())
-						,(int)Math.round(newBottomRight.getX())
-						,(int)Math.round(newBottomRight.getY()));
-				g2d.drawLine((int)Math.round(newBottomLeft.getX())
-						,(int)Math.round(newBottomLeft.getY())
-						,(int)Math.round(newBottomRight.getX())
-						,(int)Math.round(newBottomRight.getY()));
-				
-				g2d.setColor(Color.red);
-				//avg lines;
-				g2d.drawLine((int)Math.round(avgLeftLine),(int) Math.round(avgTopLine),(int) Math.round(avgRightLine),(int) Math.round(avgTopLine));
-				g2d.drawLine((int)Math.round(avgLeftLine),(int) Math.round(avgTopLine),(int) Math.round(avgLeftLine),(int) Math.round(avgBotLine));
-				g2d.drawLine((int)Math.round(avgRightLine),(int) Math.round(avgTopLine),(int) Math.round(avgRightLine),(int) Math.round(avgBotLine));
-				g2d.drawLine((int)Math.round(avgLeftLine),(int) Math.round(avgBotLine),(int) Math.round(avgRightLine),(int) Math.round(avgBotLine));
-				
-				g2d.setColor(Color.green);
+				System.out.println("method 1:");
 				System.out.println(selectedPoint);
-				g2d.fillRect((int)Math.round(selectedPoint.getX()), (int)Math.round(selectedPoint.getY()), 5, 5);
+			//	System.out.println((selectedPoint.getX()-mapLeft)/1.8 + " " + (selectedPoint.getY()-mapTop)/1.8 + "\n");
+				double actualX = (selectedPoint.getX() - mapLeft) / ((mapRight-mapLeft)/(double)Field.OUTER_BOUNDARY_WIDTH);
+				double actualY = (selectedPoint.getY() - mapTop) / ((mapBot-mapTop)/(double)Field.OUTER_BOUNDARY_HEIGHT);
+				System.out.println(actualX + " " + actualY);
+				/*
+				System.out.println("method 2:");
+				double[][] matrix = new double[3][3];
 				
-				g2d.setColor(Color.MAGENTA);
-				g2d.fillRect(x,y,5,5);
-			
-				ImageIcon ii = new ImageIcon(image);
-				JOptionPane.showMessageDialog(null,ii);
-					
+				t.getMatrix(matrix);
+				
+				double actualX = (matrix[0][0]*x + matrix[0][1]*y + matrix[0][2]) / (matrix[2][0]*x + matrix[2][1]*y + 1);
+				double actualY = (matrix[1][0]*x + matrix[1][1]*y + matrix[1][2]) / (matrix[2][0]*x + matrix[2][1]*y + 1);
+				System.out.println(actualX/1.8 + " " + actualY/1.8 + "\n");
+				*/
+				
+				
 				}	
 		}
 		this.repaint();
@@ -249,6 +140,31 @@ public class VisionPanel extends JPanel implements WebcamDisplayPanelListener{
 		}
 	}
 	
+	
+	public void createTransformMatrix() {
+		//x y: point that u want to map to
+		//xp yp: orginal points
+		tInverse = PerspectiveTransform.getQuadToQuad(mapLeft, mapTop, mapLeft, mapBot, mapRight, mapBot, mapRight, mapTop,
+				topLeft.getX(), topLeft.getY(), bottomLeft.getX(),bottomLeft.getY()
+				, bottomRight.getX(),bottomRight.getY(), topRight.getX(),topRight.getY());
+
+		System.out.println("mapping: " + mapLeft + " " + mapRight + " " + mapTop + " " + mapBot);
+		System.out.println(topLeft);
+		System.out.println(topRight);
+		System.out.println(bottomRight);
+		System.out.println(bottomLeft);
+		try {
+			t = tInverse.createInverse();
+			System.out.println(t.toString());
+	//		System.out.println(tInverse.toString());
+		} catch (NoninvertibleTransformException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (CloneNotSupportedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 	
 	
 	
