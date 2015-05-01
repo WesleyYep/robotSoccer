9bfd3d2b79cedc10fa940022a85b223e5184a6a9
Wesley <wesleyyep@hotmail.com>
2015-05-02 11:09:30 +1200

    reworked positive situation slightly

--- src/criteria/PositiveSituation.java
+++ src/criteria/PositiveSituation.java
@@ -16,17 +16,8 @@ public class PositiveSituation extends Criteria {
     @Override
     public boolean isMet() {
         Robot r = bots.getRobot(index);
-        double ballTheta = Math.atan2(r.getYPosition() - ballY, ballX - r.getXPosition());
-        double difference = ballTheta - Math.toRadians(r.getTheta());
 
-        //some hack to make the difference -Pi < theta < Pi
-        if (difference > Math.PI) {
-            difference -= (2 * Math.PI);
-        } else if (difference < -Math.PI) {
-            difference += (2 * Math.PI);
-        }
-
-        // return true if robot is at least 25cm left of ball or is pointing directly at ball and less then 25cm behind
-        return ballX - r.getXPosition() > 25 || (ballX > r.getXPosition() && Math.abs(difference) <= TurnToFaceBall.ERROR_MARGIN);
+        // return true if robot is at least 25cm left of ball or is pointing towards and less then 25cm behind
+        return ballX - r.getXPosition() > 25 || Math.abs(r.getTheta()) < 120;
     }
 }
