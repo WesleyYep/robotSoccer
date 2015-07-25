2b275b6018a1a640f0284725ad34b5e11b1430c4
Wesley <wesleyyep@hotmail.com>
2015-07-25 18:00:59 +1200

    PIDChaseBall now deals with negative situation

--- src/actions/PIDMoveToBall.java
+++ src/actions/PIDMoveToBall.java
@@ -8,12 +8,11 @@ import strategy.Action;
  */
 public class PIDMoveToBall extends Action {
 
-    private long lastTime = 0;
- //   private LimitedQueue errorsList = new LimitedQueue(10);
     private boolean isPreviousDirectionForward = true;
     private boolean isCharging = true;
     private boolean presetToForward = false;  // if true, robot will definitely go forward
     private boolean presetToBackward = false; //if true, robot will definitely go backwards
+    private double behindRange = 5;
 
     {
         parameters.put("speed", 5);
@@ -24,7 +23,7 @@ public class PIDMoveToBall extends Action {
     @Override
     public void execute() {
 
-
+        //reverse if stuck
         if (bot.isStuck(new Coordinate(bot.getXPosition(), bot.getYPosition()))) {
             if (!presetToBackward && ! presetToForward) {
                 System.out.println("bot is stuck :(");
@@ -39,36 +38,44 @@ public class PIDMoveToBall extends Action {
             presetToForward = false;
         }
 
-//        //check for obstacles
-//        for (int i = 0; i < opponentRobots.getRobots().length; i++) {
-//            Robot opp = opponentRobots.getRobot(i);
-//            if (((isPreviousDirectionForward && Math.abs(getTargetTheta(bot, opp.getXPosition(), opp.getYPosition())) < 20)
-//                    || (!isPreviousDirectionForward && Math.abs(getTargetTheta(bot, opp.getXPosition(), opp.getYPosition())) > 160))
-//                    && getDistanceToTarget(bot, opp.getXPosition(), opp.getYPosition()) < 20) {
-//                if (isPreviousDirectionForward) {
-//                    System.out.println("preset back - theta = " + Math.abs(getTargetTheta(bot, opp.getXPosition(), opp.getYPosition())) + "dist = " + getDistanceToTarget(bot, opp.getXPosition(), opp.getYPosition()));
-//                    presetToBackward = true;
-//                } else {
-//                    System.out.println("preset for");
-//                    presetToForward = true;
-//                }
-//            }
-//        }
+        double targetX = ballX;
+        double targetY = ballY;
+
+        //change target if it's a negative situation
+        if (bot.getXPosition() - ballX > behindRange) {
+            if (Math.abs(ballY - bot.getYPosition()) > 10 || bot.getXPosition() < ballX) { //bot is not inline with ball
+                behindRange = -15;
+                targetX = ballX - 20;
+            } else {
+                behindRange = -15;
+                targetX = ballX - 20;
+                if (ballY > 110) {
+                    targetY = ballY - 20;
+                } else {
+                    targetY = ballY + 20;
+                }
+            }
+            if (ballX < 20) {
+                if (ballY > 45 && ballY < 135) {
+                    bot.linearVelocity = 0;
+                    return;//don't go for it!
+                } else {
+                    targetX = ballX;
+                    targetY = ballY;
+                }
+            }
+        } else {
+            behindRange = 5;
+        }
 
         boolean isCurrentDirectionForward;
-        double timePeriod;
-        long currentTime = System.currentTimeMillis();
-
-        timePeriod = lastTime == 0 ? 0 : currentTime - lastTime;
-        lastTime = currentTime;
-
         //get angle to ball
-        double angleToBall = getTargetTheta(bot, ballX, ballY);
+        double angleToBall = getTargetTheta(bot, targetX, targetY);
         double actualAngleError;
-        double distanceToBall = getDistanceToTarget(bot, ballX, ballY);
+        double distanceToBall = getDistanceToTarget(bot, targetX, targetY);
 
         //spin if ball is stuck beside robot on positive side
-        if (distanceToBall < 7 && Math.abs((angleToBall-90)%180) < 30 && ballX > bot.getXPosition()) {
+        if (distanceToBall < 7 && Math.abs((angleToBall-90)%180) < 30 && targetX > bot.getXPosition()) {
             if (angleToBall > 0) {//ie. around 90
                 bot.angularVelocity = 30;
             } else {
@@ -92,13 +99,7 @@ public class PIDMoveToBall extends Action {
             bot.linearVelocity = parameters.get("speed")/10.0;
             isCurrentDirectionForward = true;
         }
-//        if (isCurrentDirectionForward == isPreviousDirectionForward) {
-//            errorsList.add(actualAngleError * timePeriod/1000.0);
-//        } else {
-//            errorsList.clear();
-//        }
         isPreviousDirectionForward = isCurrentDirectionForward;
-//        bot.angularVelocity += errorsList.getTotal() * parameters.get("ki");
 
         //charge ball into goal
         double range = 10;
@@ -107,7 +108,7 @@ public class PIDMoveToBall extends Action {
         }
         if (distanceToBall < range && Math.abs(actualAngleError) < Math.PI/10 /* radians*/) {
             bot.linearVelocity = isCurrentDirectionForward ? 1 : -1;
-            if (ballX > 110) {
+            if (targetX > 110) {
                 double angleToGoal = angleDifferenceFromGoal(bot.getXPosition(), bot.getYPosition(), bot.getTheta()); //degrees
                 if (Math.abs(angleToGoal) > 45) {
                     if (angleToGoal > 0 && isCurrentDirectionForward || angleToGoal < 0 && !isCurrentDirectionForward) {
@@ -117,9 +118,6 @@ public class PIDMoveToBall extends Action {
                     }
                 }
             }
-//            if (bot.getXPosition() > 140) {
-//                bot.angularVelocity = 30;
-//            }
             isCharging = true;
         } else {
             isCharging = false;
