681ecfb1d1536393b2b3169f6cdfe39637d5ffe9
Wal8800 <johnlaw1234@hotmail.com>
2015-07-31 08:19:45 +1200

    added old program movement for testing

--- src/actions/MoveToFacing.java
+++ src/actions/MoveToFacing.java
@@ -18,8 +18,22 @@ public class MoveToFacing  extends Action {
     private boolean stationary= false;
     private double previousX = 0;
     private double previousY = 0;
-    private double highestY = 0;
-    private double lowestY = 180;
+    
+    double m_VcValue = 2.4500000000000002;
+    double m_K_Vc = 0.050000000000000003;
+    double m_VmaxMin = 0;
+    double m_VmaxMax = 0.8;
+    double m_K_Vmax = 23.5;
+    double m_dVMin = 0;
+    double m_dVMax = 425.0;
+    double m_K_dV = 43.119999999999997;
+    double m_Ak1 = 12.119999999999999;
+    double m_Ak2 = 19.940000000000001;
+    double m_Ak3 =3600.0000000000000;
+	private double pastLinearVelocity = 0;
+	private double pastAngularVelocity = 0;
+    
+    
     //non-static initialiser block
     {
         parameters.put("fixed point1 x", 140);
@@ -33,252 +47,45 @@ public class MoveToFacing  extends Action {
         //System.out.println("obs");
         int x = parameters.get("fixed point1 x");
         int y = parameters.get("fixed point1 y");
-        //x = (int) Math.round(ballX);
-       // y = (int)Math.round(ballY);
         double angle = parameters.get("direction facing");
-        //setVelocityToTarget(140,30, 0);
-
-        double yDiff = y-15*Math.sin(Math.toRadians(angle*-1));
-        double xDiff = x-15*Math.cos(Math.toRadians(angle*-1));
-        double robotPosition =  Math.toDegrees(Math.atan2(r.getYPosition() - y, x - r.getXPosition()));
-
-        if (angle > 0) {
-            robotPosition = robotPosition + (180-angle);
-        }
-        else if (angle < 0) {
-            robotPosition += (-180-angle);
-        }
-        else {
-            robotPosition += 180;
-        }
-
-        if (robotPosition >= 180) {
-            robotPosition -= 360;
-        }
-
-        if (robotPosition <= -180) {
-            robotPosition += 360;
-        }
-        robotPosition *= -1;
-
-        double tempAngle = 0;
-        if (Math.abs(robotPosition) > 160) {
-            tempAngle = angle;
+        
+        if (ballY >= 70 && ballY < 110) {
+        	setVelocityToTarget(7,ballY);
+        } else if (ballY < 70)  {
+        	setVelocityToTarget(7,70);
         } else {
-            if (robotPosition > 0) {
-                tempAngle = angle+ 35;
-            } else {
-                tempAngle = angle - 35;
-            }
+        	setVelocityToTarget(7,110);
         }
-
-        if (tempAngle >= 180) {
-            tempAngle -= 360;
-        }
-
-        if (tempAngle <= -180) {
-            tempAngle += 360;
-        }
-
-
-        double yTurn  = yDiff-20*Math.sin(Math.toRadians(tempAngle*-1));
-        double xTurn  = xDiff-20*Math.cos(Math.toRadians(tempAngle*-1));
-        if (Math.abs(robotPosition) > 165) {
-            yTurn = yDiff;
-            xTurn = xDiff;
-        }
-
-        //if (once) {
-        pointList.clear();
-        pointList.add(new Point(xTurn,yTurn));
-        pointList.add(new Point(xDiff,yDiff));
-        pointList.add(new Point(x,y));
-        //pointList.add(new Point(330,35));
-        //  once = false;
-        // }
-
-        Point currentPoint = pointList.get(currentPointIndex);
-        double targetDist = Math.sqrt(Math.pow((currentPoint.x-r.getXPosition()),2) + Math.pow((currentPoint.y-r.getYPosition()),2));
-        if (stationary && targetDist >= 3.75) {
-            currentPointIndex = 0;
-            stationary = false;
-        }
-
-        if (targetDist < 5) {
-            currentPointIndex++;
-            if (currentPointIndex == pointList.size()) {
-                //System.out.println("here");
-                currentPointIndex--;
-            }
-            currentPoint = pointList.get(currentPointIndex);
-        }
-        //System.out.println(currentPointIndex);
-        setVelocityToTarget(currentPoint.x,currentPoint.y);
-
-        if (r.getXPosition() == previousX && r.getYPosition() == previousY) {
-            stationary = true;
-        }
-        r.linearVelocity = 1;
-        r.angularVelocity = Math.PI/2;
-        //System.out.println(stationary + " " + currentPointIndex);
-
+        
+       
+        this.pastAngularVelocity = r.angularVelocity;
+        this.pastLinearVelocity = r.linearVelocity;
     }
 
-    /*
-    public void setVelocityToTarget(double x, double y, double direction) {
-        Robot r = bot;
-
-        double targetDist,robotPosition,robotDirection;
-        targetDist = Math.sqrt(Math.pow((x-r.getXPosition()),2) + Math.pow((y-r.getYPosition()),2));
-        robotDirection = r.getTheta() - direction;
-        
-        if (robotDirection >= 270) {
-        	robotDirection -= 360;
-        }
-        
-        if (robotDirection <= -270) {
-        	robotDirection += 369;
-        }
-        
-        
-        robotPosition =  Math.toDegrees(Math.atan2(r.getYPosition() - y, x - r.getXPosition()));
-        
-        if (direction > 0) {
-        	robotPosition = robotPosition + (180-direction);
-        }
-        else if (direction < 0) {
-        	robotPosition += (-180-direction);
-        }
-        else {
-        	robotPosition += 180;
-        }
-        
-        if (robotPosition >= 180) {
-        	robotPosition -= 360;
-        }
-        
-        if (robotPosition <= -180) {
-        	robotPosition += 360;
-        } 
-        robotPosition *= -1;
-        System.out.println(robotDirection +" " +robotPosition);
-        
-        
-        FunctionBlock fb = loadFuzzy("moveToSpotDirection.fcl");
-
-        fb.setVariable("targetDist", Math.abs(targetDist));
-        fb.setVariable("robotPosition", robotPosition);
-        fb.setVariable("robotDirection", robotDirection*-1);
-        // Evaluate
-        fb.evaluate();
-        
-        
-           JFuzzyChart.get().chart(fb);
-        // Show output variable's chart
-       fb.getVariable("linearVelocity").defuzzify();
-       fb.getVariable("angularVelocity").defuzzify();
-          JFuzzyChart.get().chart(fb.getVariable("linearVelocity"), fb.getVariable("linearVelocity").getDefuzzifier(), true);
-       JFuzzyChart.get().chart(fb.getVariable("angularVelocity"), fb.getVariable("angularVelocity").getDefuzzifier(), true);
-        JOptionPane.showConfirmDialog(null, "wait");     
-        
-        
-        double linear  = fb.getVariable("linearVelocity").getValue();
-        double angular = fb.getVariable("angularVelocity").getValue();
-
-
-        r.linearVelocity = linear;
-        r.angularVelocity = angular;
-
-       // System.out.println(linear + " " + angular*-1);
-        
-        if (targetDist <=3.75) {
-            r.linearVelocity = 0;
-            r.angularVelocity = 0;
-        } 
-        return;
-    } */
 
     public void setVelocityToTarget(double x, double y) {
-        Robot r = bot;
-        double obstacleTheta = 180, obstacleDist = 220, obstacleX = 0, obstacleY = 0;
-        for (int i=0; i<5; i++) {
-            if (!teamRobots.getRobot(i).equals(r)) {
-                Robot obs = teamRobots.getRobot(i);
-                double tempTheta = Math.atan2(r.getYPosition() - obs.getYPosition(), obs.getXPosition() - r.getXPosition());
-                double tempDifference = tempTheta - Math.toRadians(r.getTheta());
-                if (tempDifference > Math.PI) {
-                    tempDifference -= (2 * Math.PI);
-                } else if (tempDifference < -Math.PI) {
-                    tempDifference += (2 * Math.PI);
-                }
-                tempDifference = Math.toDegrees(tempDifference);
-                tempTheta = tempDifference;
-
-                if (Math.abs(tempTheta) < 50) {
-                    double tempDist = Math.sqrt(Math.pow((obs.getXPosition()-r.getXPosition()),2) + Math.pow((obs.getYPosition()-r.getYPosition()),2));
-                    if (tempDist < obstacleDist) {
-                        obstacleDist = tempDist;
-                        obstacleTheta = tempTheta;
-                        obstacleY = obs.getYPosition();
-                        obstacleX = obs.getXPosition();
-                    }
-                }
-            }
-        }
-
-        for (int i=0; i<5; i++) {
-            Robot obs = opponentRobots.getRobot(i);
-            double tempTheta = Math.atan2(r.getYPosition() - obs.getYPosition(), obs.getXPosition() - r.getXPosition());
-            double tempDifference = tempTheta - Math.toRadians(r.getTheta());
-            if (tempDifference > Math.PI) {
-                tempDifference -= (2 * Math.PI);
-            } else if (tempDifference < -Math.PI) {
-                tempDifference += (2 * Math.PI);
-            }
-            tempDifference = Math.toDegrees(tempDifference);
-            tempTheta = tempDifference;
-
-            if (Math.abs(tempTheta) < 50) {
-                double tempDist = Math.sqrt(Math.pow((obs.getXPosition()-r.getXPosition()),2) + Math.pow((obs.getYPosition()-r.getYPosition()),2));
-                if (tempDist < obstacleDist) {
-                    obstacleDist = tempDist;
-                    obstacleTheta = tempTheta;
-                    obstacleY = obs.getYPosition();
-                    obstacleX = obs.getXPosition();
-                }
-            }
-        }
-
-
-        /*
-        if (r.getXPosition() - ballX > 25|| Math.abs(r.getTheta()) > 60) {
-            double tempTheta = Math.atan2(r.getYPosition() - ballY, ballX - r.getXPosition());
-            double tempDifference = tempTheta - Math.toRadians(r.getTheta());
-            if (tempDifference > Math.PI) {
-                tempDifference -= (2 * Math.PI);
-            } else if (tempDifference < -Math.PI) {
-                tempDifference += (2 * Math.PI);
-            }
-            tempDifference = Math.toDegrees(tempDifference);
-            tempTheta = tempDifference;
-
-            if (Math.abs(tempTheta) < 50) {
-                double tempDist = Math.sqrt(Math.pow((obs.getXPosition()-r.getXPosition()),2) + Math.pow((obs.getYPosition()-r.getYPosition()),2));
-                if (tempDist < obstacleDist) {
-                    obstacleDist = tempDist;
-                    obstacleTheta = tempTheta;
-                    obstacleY = obs.getYPosition();
-                    obstacleX = obs.getXPosition();
-                }
-            }
-        } */
-
-        double targetDist;
-        double targetTheta = Math.atan2(r.getYPosition() - y, x - r.getXPosition());
-        double difference = targetTheta - Math.toRadians(r.getTheta());
-//       System.out.println("initial targetTheta: " + targetTheta + " initial difference " + difference + " current Theta " 
-        //     		+ Math.toRadians(r.getTheta()));
-        //some hack to make the difference -Pi < theta < Pi
+    	Robot r = bot;
+    	Point p = ExecuteMission_MoveTo(4,new Point(x,y), true);
+    	
+    	
+    	
+    	if (Math.sqrt(Math.pow((p.x),2) + Math.pow(p.y,2)) < 3.75) {
+    		if (ExecuteMission_OrientationTo(4,90, false) < 5) {	
+    			r.linearVelocity = 0;
+    			r.angularVelocity = 0;
+ 
+    		}
+    		
+    	} 
+    	System.out.println(r.linearVelocity + " " + r.angularVelocity + " " + Math.sqrt(Math.pow((p.x),2) + Math.pow(p.y,2)));
+       
+    }
+    
+    private double ExecuteMission_OrientationTo(int VP2M_id, int orientation, boolean bMoveForwardOnly) {
+		
+    	double targetDist = 0;
+    	double targetTheta = orientation;
+        double difference = Math.toRadians(targetTheta) - Math.toRadians(bot.getTheta());
         if (difference > Math.PI) {
             difference -= (2 * Math.PI);
         } else if (difference < -Math.PI) {
@@ -286,76 +93,143 @@ public class MoveToFacing  extends Action {
         }
         difference = Math.toDegrees(difference);
         targetTheta = difference;
-        /*
-        obstacleTheta = Math.atan2(r.getYPosition() - obstacleY, obstacleX - r.getXPosition());
-        double obsDifference = obstacleTheta - Math.toRadians(r.getTheta());
-        if (obsDifference > Math.PI) {
-        	obsDifference -= (2 * Math.PI);
-        } else if (obsDifference < -Math.PI) {
-        	obsDifference += (2 * Math.PI);
+        
+        boolean bMoveBackward = false;
+        
+        if (!bMoveForwardOnly && Math.abs(targetTheta) > 90) {
+            if (targetTheta < 0) {
+                targetTheta = -180 - targetTheta;
+            }
+            else if (targetTheta > 0) {
+                targetTheta = 180 - targetTheta;
+            }
         }
-        obsDifference = Math.toDegrees(obsDifference);
-        obstacleTheta = obsDifference;*/
-
-        targetDist = Math.sqrt(Math.pow((x-r.getXPosition()),2) + Math.pow((y-r.getYPosition()),2));
-        // obstacleDist = Math.sqrt(Math.pow((obstacleX-r.getXPosition()),2) + Math.pow((obstacleY-r.getYPosition()),2));
-        double tempAngle = r.getTheta();
-
-        /*
-        if (tempAngle < -270) {
-            tempAngle = 0;
-        }*/
-
-        double distBetweenObsAndTarget = Math.sqrt(Math.pow((x-obstacleX),2) + Math.pow((y-obstacleY),2));
-        FunctionBlock fb = loadFuzzy("fuzzy/selfMadeObstacle.fcl");
-        fb.setVariable("distBetweenObsAndTarget", distBetweenObsAndTarget);
-        fb.setVariable("obstacleDist", obstacleDist);
-        fb.setVariable("obstacleTheta", obstacleTheta);
-        fb.setVariable("targetTheta", targetTheta);
-        fb.setVariable("targetDist", Math.abs(targetDist));
-        fb.setVariable("direction", tempAngle);
-        fb.setVariable("xPos", r.getXPosition());
-        fb.setVariable("yPos", r.getYPosition());
-        //      System.out.println("x y: " + x + " " + y + " r.x r.y " + r.getXPosition() + " "
-        //      		+ r.getYPosition() + " targetDist " + targetDist);
-        // Evaluate
-        fb.evaluate();
-        /*
-           JFuzzyChart.get().chart(fb);
-
-           
-        // Show output variable's chart
-       fb.getVariable("linearVelocity").defuzzify();
-       fb.getVariable("angularVelocity").defuzzify();
-          JFuzzyChart.get().chart(fb.getVariable("linearVelocity"), fb.getVariable("linearVelocity").getDefuzzifier(), true);
-       JFuzzyChart.get().chart(fb.getVariable("angularVelocity"), fb.getVariable("angularVelocity").getDefuzzifier(), true);
-        JOptionPane.showMessageDialog(null, "nwa"); */
-        double linear  = fb.getVariable("linearVelocity").getValue();
-        double angular = fb.getVariable("angularVelocity").getValue();
-        //    System.out.println(" raw right :" + fb.getVariable("rightWheelVelocity").getValue() + " raw left " + fb.getVariable("leftWheelVelocity").getValue());
-
-        //    System.out.println("right :" + right + "left " + left); 
-
-        r.linearVelocity = linear;
-        r.angularVelocity = angular*-1;
-//            System.out.println("linear: " + linear + " angular:" + angular*-1
-//            					+ " x: " + r.getXPosition() + " y: " + r.getYPosition()
-//            					+ " r theta: " + r.getTheta() + " t theta: " + targetTheta
-//            					+ " t dist" + targetDist + " time: " + System.currentTimeMillis());
-
-        if (targetDist <=3.75) {
-            r.linearVelocity = 0;
-            r.angularVelocity = 0;
+        
+        GenerateVelocity_VP2(VP2M_id, targetTheta, targetDist, bot);
+        
+        
+        if (bMoveBackward) {
+       	 bot.linearVelocity *= -1;
         }
-        //   System.out.println("linear velocity " + r.linearVelocity + " angular velocity" + r.angularVelocity + "angleError: " + targetTheta
-        //		 + " r.angle: " + r.getTheta() + " dist: " + targetDist);
+        
+        SmoothVelocity(VP2M_id);
+		return Math.abs(targetTheta);
+	}
 
-        //    System.out.println("linear: " + r.linearVelocity + " y: " + y + " theta: " + targetTheta + " dist: " + targetDist);
-//             r.linearVelocity = 0;
-//            r.angularVelocity = 0;
-//        	
 
-        // }
-        return;
+	public Point ExecuteMission_MoveTo(int VP2M_id, Point pos, boolean bMoveForwardOnly) {
+    	 Robot r = bot;
+    	 double tempX = r.getXPosition() + (r.linearVelocity*0.100);
+    	 
+    	 double  targetDist = Math.sqrt(Math.pow((pos.x-r.getXPosition()),2) + Math.pow((pos.y-r.getYPosition()),2));
+         double targetTheta = Math.atan2(r.getYPosition() - pos.y, pos.x - r.getXPosition());
+         double difference = targetTheta - Math.toRadians(r.getTheta());
+         if (difference > Math.PI) {
+             difference -= (2 * Math.PI);
+         } else if (difference < -Math.PI) {
+             difference += (2 * Math.PI);
+         }
+         difference = Math.toDegrees(difference);
+         targetTheta = difference;
+         
+         boolean bMoveBackward = false;
+         
+         if (!bMoveForwardOnly && Math.abs(targetTheta) > 90) {
+             if (targetTheta < 0) {
+                 targetTheta = -180 - targetTheta;
+             }
+             else if (targetTheta > 0) {
+                 targetTheta = 180 - targetTheta;
+             }
+         }
+         
+         GenerateVelocity_VP2(VP2M_id, targetTheta, targetDist, r);
+         
+         
+         if (bMoveBackward) {
+        	 r.linearVelocity *= -1;
+         }
+         
+         SmoothVelocity(VP2M_id);
+         
+         return new Point(pos.x - r.getXPosition(), pos.y - r.getYPosition());
     }
+
+	private void SmoothVelocity(int vP2M_id) {
+		Robot r = bot;
+		double LinearVelocityMaxAcc  = m_Ak1 * 1.0 / 30.0;
+		double LinearVelocityMaxDec  = m_Ak2 * 1.0 / 30.0;
+		double AngularVelocityMaxAcc = m_Ak3 * Math.PI / 180.0 * 1.0 / 30.0;
+		
+		double pre_lv = pastLinearVelocity;
+		double pre_av = pastAngularVelocity;
+
+		if( pre_lv * r.linearVelocity < 0 )
+		{
+			r.linearVelocity = 0;
+		}
+		else
+		{
+			double d_lv = r.linearVelocity - pre_lv;
+			if( d_lv > 0  )  {
+				d_lv = Math.min( d_lv, LinearVelocityMaxAcc );
+
+				//if( r.linearVelocity > 0 )
+					r.linearVelocity = pre_lv + d_lv;
+				//else
+				//	r.linearVelocity = pre_lv - d_lv;
+			}
+			else 
+			{
+				d_lv = -Math.min( -d_lv, LinearVelocityMaxDec );
+
+				r.linearVelocity = pre_lv + d_lv;
+			}
+		}
+
+		if( pre_av * r.angularVelocity < 0 )
+		{
+			r.angularVelocity = 0;
+		}
+		else
+		{
+			double d_av = r.angularVelocity - pre_av;
+			if( d_av > 0  ) 
+			{
+				d_av = Math.min( d_av, AngularVelocityMaxAcc );
+
+				//if( r.angularVelocity > 0 )
+					r.angularVelocity = pre_av + d_av;
+				//else
+				//	r.angularVelocity = pre_av - d_av;
+			}
+			else 
+			{
+				d_av = -Math.min( -d_av, AngularVelocityMaxAcc );
+
+				//if( r.angularVelocity > 0 )
+					r.angularVelocity = pre_av + d_av;
+				//else
+				//	r.angularVelocity = pre_av - d_av;
+			}
+		}
+	}
+
+	private void GenerateVelocity_VP2(int vP2M_id, double targetTheta,
+			double targetDist, Robot r) {
+		
+		double expEquation =  -(targetTheta/m_K_dV)*(targetTheta/m_K_dV); 
+		double AngularVelocityDegree = (m_dVMin +  (m_dVMax - m_dVMin)*(1.0 - Math.exp(expEquation)));
+		
+		if (targetTheta < 0 ) AngularVelocityDegree *= -1;
+		double expEquation2 = -(targetTheta/m_K_Vmax)*(targetTheta/m_K_Vmax);  
+		double Vmax = (m_VmaxMin +  (m_VmaxMax-m_VmaxMin)*(Math.exp(expEquation2)));
+		
+		double expEquation3 = -(targetDist/m_K_Vc)*(targetDist/m_K_Vc); 
+		double V = (Vmax*(1.0 -Math.exp(expEquation3)));
+		
+		r.angularVelocity = AngularVelocityDegree *Math.PI/180;
+		r.linearVelocity = V;
+	}
+    
 }
