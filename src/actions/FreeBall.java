e35b2bbdfbe6ed49ee28fa135808f9b584c273d9
Wesley <wesleyyep@hotmail.com>
2015-10-06 23:54:09 +1300

    possible free ball action

--- src/actions/FreeBall.java
+++ src/actions/FreeBall.java
@@ -14,13 +14,23 @@ public class FreeBall extends Action {
 
     @Override
     public void execute() {
-        int speed = parameters.get("speed");
+//        int speed = parameters.get("speed");
+//
+//        if (speed > 100) {
+//            speed = -(speed - 100);
+//        }
+//        bot.linearVelocity = speed;
+//        bot.angularVelocity = 10;
 
-        if (speed > 100) {
-            speed = -(speed - 100);
+        double distanceToBall = getDistanceToTarget(bot, ballX, ballY);
+
+        if (distanceToBall < 10) {
+            bot.angularVelocity = 30;
+            bot.linearVelocity = 0;
+        } else {
+            bot.linearVelocity = 3;
+            bot.angularVelocity = 0;
         }
-        bot.linearVelocity = speed;
-        bot.angularVelocity = 10;
 
     }
 
