661c70e379478837ea185dca6061d09d34565fee
BLAZE <chan743@en-330393.UoA.auckland.ac.nz>
2015-10-07 16:27:10 +1300

    Less unncessary turning in pid

--- src/actions/TestPIDMoveToBall.java
+++ src/actions/TestPIDMoveToBall.java
@@ -6,7 +6,7 @@ import strategy.Action;
 public class TestPIDMoveToBall extends Action {
 
 	private PIDController linearPID = new PIDController(1, 1, 1);
-	private PIDController angularPID = new PIDController(2, 0, 0);
+	private PIDController angularPID = new PIDController(3, 0, 0);
 	
 	private double previousBallX;
     private double previousBallY;
@@ -17,9 +17,9 @@ public class TestPIDMoveToBall extends Action {
 
     public TestPIDMoveToBall() {
         // set upper and lower bounds
-        angularPID.setMaximumOutput(Math.toRadians(180));
-        angularPID.setMinimumOutput(Math.toRadians(-180));
-        angularPID.setClip(false);
+        angularPID.setMaximumOutput(Math.toRadians(120));
+        angularPID.setMinimumOutput(Math.toRadians(-120));
+        angularPID.setClip(true);
     }
 
 	@Override
@@ -36,9 +36,38 @@ public class TestPIDMoveToBall extends Action {
 
         double difference = Math.abs(Math.abs(previousBallY) - Math.abs(ballY) + Math.abs(previousBallX) - Math.abs(ballX));
 
+        double correctAngle = 0;
+
+        //  a                   | b
+        //                      |
+        // 180                  |                       0
+        // ----------------------------------------------
+        // -180                 |                       0
+        //                      |
+        //  c                   | d
+
+        if (angleToBall > 90) {
+            // quadrant a
+
+            correctAngle = 180 - angleToBall;
+            // ball is behind
+            bot.linearVelocity = -0.8;
+        } else if (angleToBall < 0 && angleToBall > -90) {
+            // quadrant d
+            correctAngle = Math.abs(angleToBall);
+            bot.linearVelocity = 0.8;
+        } else if (angleToBall > 0 && angleToBall < 90) {
+            // quadrant b
+            correctAngle = angleToBall * -1;
+            bot.linearVelocity = 0.8;
+        } else {
+            bot.linearVelocity = -0.8;
+            correctAngle = -180 - angleToBall;
+        }
+
         // update goal
         if (difference > ballErrorMargin) {
-            angularPID.setResult(Math.toRadians(angleToBall));
+            angularPID.setResult(Math.toRadians(correctAngle));
             // clear previous error
             angularPID.setTotalError(0);
             System.out.println("Goal changed");
@@ -46,25 +75,22 @@ public class TestPIDMoveToBall extends Action {
 
         //linearPID
         // update current state
-        angularPID.setInput(Math.toRadians(angleToBall));
+        angularPID.setInput(Math.toRadians(correctAngle));
 
-        System.out.println("Angle: " + angleToBall);
+        System.out.println("Angle: " + correctAngle);
         // testing purpose
         double result = angularPID.performPID();
 
-        if (Math.abs(result) < 0.7) {
-            bot.angularVelocity = 0;
-        } else {
-            bot.angularVelocity = result;
-        }
+        bot.angularVelocity = result;
 
-//        if (result < 0) {
-//            bot.linearVelocity = -0.5;
-//        } else {
-//            bot.linearVelocity = 0.5;
-//        }
-        bot.linearVelocity = 0.5;
-        System.out.println("Angular v: " + bot.angularVelocity);
+        // override linear velocity if ball distance is close and is turning
+        if (Math.abs(bot.angularVelocity) > 1 && distanceToBall < 20) {
+            bot.linearVelocity = 0.1;
+        } else if (distanceToBall > 70 && bot.linearVelocity < 0) {
+            bot.linearVelocity = -1.3;
+        } else if (distanceToBall > 70 && bot.linearVelocity > 0) {
+            bot.linearVelocity = 1.3;
+        }
 
         // update previous
         previousBallX = ballX;
